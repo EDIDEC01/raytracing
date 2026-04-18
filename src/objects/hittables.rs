78e049c8a70e86827fc5ebd0486a67c74cc4b0e9
Edi De Candido <decandidoedi@gmail.com>
2026-04-18 10:13:38 +0200

    implementation of the HittableList

---
+++ src/objects/hittables.rs
@@ -0,0 +1,63 @@
+use crate::types::point::Point;
+use crate::types::vector::Vec3;
+use crate::types::ray::Ray;
+
+#[derive(Default, Clone)]
+pub struct HitRecord {
+    pub p: Point,
+    pub normal: Vec3,
+    pub t: f64,
+    pub front_face: bool
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        self.front_face = r.dir().dot(*outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord>;
+}
+
+pub struct HittableList {
+    pub objects: Vec<Box<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        Self { objects: Vec::new() }
+    }
+
+    pub fn add(&mut self, object: Box<dyn Hittable>) {
+        self.objects.push(object);
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        let mut hit_anything = None;
+        let mut closest_so_far = ray_tmax;
+
+        for object in &self.objects {
+            if let Some(temp_rec) = object.hit(r, ray_tmin, closest_so_far) {
+                closest_so_far = temp_rec.t;
+                hit_anything = Some(temp_rec);
+            }
+        }
+
+        hit_anything
+    }
+}
